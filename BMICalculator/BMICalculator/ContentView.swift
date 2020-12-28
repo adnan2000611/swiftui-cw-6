dacc77726922a48f5462833c74b3f65b45356811
Adnan Alfadhli <adnan2000611@gmail.com>
2020-12-29 00:14:52 +0300

    cal done

--- BMICalculator/BMICalculator/ContentView.swift
+++ BMICalculator/BMICalculator/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = "Adnan"
+   // @Binding var BMI: String
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            NavigationView{
+                VStack{
+                    
+                    
+                    Text("My name : \(name)").modifier(BMITextFieldMod())
+                    Text("BMI: ").modifier(BMITextFieldMod())
+                    Text("Status: ").modifier(BMITextFieldMod())
+                    NavigationLink(
+                        destination: BMI(),
+                        label: {
+                            Text("Press").padding().foregroundColor(.white).background(Color(#colorLiteral(red: 0.2558337152, green: 0.583994925, blue: 0.9943234324, alpha: 1))).cornerRadius(25)
+                        })
+                    
+                        
+                    
+                        
+                    
+                }
+                
+            }
+        }
+        
+    }
+}
+struct BMI: View {
+    @State var height: String = ""
+    @State var weight: String = ""
+    @State var bmi: Double = 0
+    var body: some View{
+        VStack{
+            
+            Text("مرحبا بك بحاسبه الBMI").modifier(BMITextFieldMod())
+            TextField("height", text: $height)
+            TextField("weight", text: $weight)
+            Button(action: {
+                
+                bmi = BMICAL(height: height, weight: weight)
+                height = ""
+                weight = ""
+                
+            }, label: {
+                Text("احسب").modifier(BMITextFieldMod())
+            })
+            
+            
+            Text(String(bmi)).modifier(BMITextFieldMod())
+        }
     }
 }
 
+func BMICAL(height: String , weight: String) -> Double {
+    let hInt = Double(height)!
+    let wInt = Int(weight)!
+    
+    let bmi = Double(wInt) / ( hInt*hInt )
+    return bmi
+}
+
+struct BMITextFieldMod : ViewModifier {
+    func body(content: Content) -> some View {
+        content.font(.system(size: 40, weight: .bold, design: .rounded))
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        BMI()
     }
 }
